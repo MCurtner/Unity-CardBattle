d75e435642277052c8aa1eee1ecb7a57e9cae1a1
Matthew Curtner <Kinematic12@gmail.com>
2017-03-26 20:09:15 +0200

    Card Updates

--- Assets/Scripts/UI Scripts/EndTurnButton.cs
+++ Assets/Scripts/UI Scripts/EndTurnButton.cs
@@ -5,7 +5,7 @@ public class EndTurnButton : MonoBehaviour {
 
     public void OnClick()
     {
-            TurnManager.Instance.EndTurn();
+            //TurnManager.Instance.EndTurn();
     }
 
 }
